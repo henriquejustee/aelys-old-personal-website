7afd19629b62538ea6d4ac46a489aa160b233ada
Aelys <AelysCh@proton.me>
2023-11-26 19:41:10 -0300

    Remove unused dependencies and update website description

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
             <div className="backdrop-blur-xl drop-shadow-md p-7 rounded-t-lg shadow-lg max-w-2xl">
                 <img className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 rounded-full border-solid border-2 border-purple-500 w-32 h-32 mx-auto m-3" src="pfp.png" alt="Profile picture" />
                 <h1 className="text-white text-4xl font-bold mb-4 text-center">Welcome to my website :3</h1>
-                <p className="text-white">This website was made with <span className="font-extrabold"><a href="https://www.typescriptlang.org/">Typescript</a></span>, React and TailwindCSS, Here will you can check my socials, stack, projects, curriculum and etc!</p>
+                <p className="text-white">This website was made with <span className="font-extrabold"><a href="https://www.typescriptlang.org/">Typescript</a></span>, <span><a className="font-extrabold" href='https://react.dev/'>React</a></span> and <span><a className="font-extrabold" href='https://tailwindcss.com/'>TailwindCSS</a></span>, Here you can check my socials, stack, projects, curriculum and etc!</p>
             </div>
     
             <div className="flex justify-center space-x-4 mt-4">
