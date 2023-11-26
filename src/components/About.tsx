91a28b3ebb46ec697e7883065d4049cf6c3a9730
Aelys <AelysCh@proton.me>
2023-11-26 19:14:59 -0300

    Update title and styling in index.html and About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,9 +4,9 @@ export default function AboutMe() {
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 h-full">
           <div className="h-full">
-            <img className="w-full h-full object-cover object-top" src="https://images.pexels.com/photos/270373/pexels-photo-270373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            <img className="w-full h-full object-cover object-top rounded-s-xl" src="https://images.pexels.com/photos/270373/pexels-photo-270373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
           </div>
-          <div className="flex bg-gray-100 p-10 w-full md:w-auto">
+          <div className="flex bg-gray-100 p-10 w-full md:w-auto rounded-e-xl opacity-80">
             <div className="">
               <h1 className="text-3xl uppercase">Aelys</h1>
               <p className="font-semibold mb-5">Backend Developer</p>
