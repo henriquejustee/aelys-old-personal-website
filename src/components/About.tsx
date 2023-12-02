42c3df142b58e479168fbcc01dfd6cd8a9d11b55
Aelys <AelysCh@proton.me>
2023-12-01 23:20:45 -0300

    Refactor AboutMe and Footer components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,17 +3,19 @@ import { FaCat } from "react-icons/fa";
 
 export default function AboutMe() {
   return (
-    <div className="flex justify-center font-ubuntu animate-fade-in bg-gray-100 py-8 bg-cover bg-fixed bg-[url('../mountain_sunset.jpg')]" style={{ minHeight: 'calc(100vh - 64px)' }}>
+    <div className="flex justify-center items-center sm:p-5 font-ubuntu animate-fade-in bg-cover bg-fixed bg-[url('../mountain_sunset.jpg')]" style={{ minHeight: 'calc(100vh - 64px)' }}>
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2">
           <div className="h-full">
-            <img className="opacity-80 md:ml-auto sm:hover:opacity-100 sm:hover:transition ease-in-out delay-150 sm:hover:-translate-y-1 sm:hover:scale-110 duration-300 " src="pfp2.png" height={300} width={500} alt="aelys_char" />
+            <img className="opacity-80 md:ml-auto sm:justify-end sm:hover:opacity-100 mr-24 sm:hover:transition ease-in-out delay-150 sm:hover:-translate-y-1 sm:hover:scale-110 duration-300 " src="pfp2.png" height={400} width={600} alt="aelys_char" />
           </div>
           <div className="mr-auto flex bg-gradient-to-r from-slate-800 to-transparent p-10  rounded opacity-90">
             <div className="">
-              <h1 className="text-white text-3xl uppercase"><FaCat />Aelys</h1>
-              <p className="text-white font-semibold mb-5">Backend Developer</p>
-              <p className="text-white">I'm a programming and information technology student with two years of experience
+              <h1 className="text-white text-4xl uppercase"><FaCat />Aelys</h1>
+              <p className="text-white font-semibold mb-5 text-2xl">Backend Developer</p>
+              <h3 className="text-white font-bold">When I first interacted with a computer, I was always been curious about how things are made from scratch, whether it be hardware or the very pixel you see in screens, that's when I started to slowly fall in love with tech.</h3>
+              <p className="text-white pt-2">A hot take, I firmly believe that in order to hone your craft, you'll have to sacrifice some of your free time. Fortunately, in my case, programming is my hobby and it just happens to be my profession.</p>
+              <p className="text-white pt-2">I'm a programming and information technology student with two years of experience
                   in frontend and backend projects. At the age of 16, I turned programming into both a
 hobby and a regular part of my routine, discovering my preference for backend
 development as it allows me to better filter and focus my efforts.
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,25 @@
+
+
+
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-8">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center">
-          <span className="ml-2 text-lg font-semibold">Hosted by Vercel </span>
-          <span className='pl-3 font-light inline-block'>and made with love by Aelys</span>
+    <footer className="flex flex-col bg-neutral-900 text-center items-center text-white ">
+      <div className="container p-6 text-white">
+        <div className="grid gap-4 lg:grid-cols-2">
+          <div className="mb-6 md:mb-0">
+            <h5 className="mb-2 font-medium uppercase">About this website</h5>
+            <p className="mb-4">
+            The website has the purpose of bringing together my work, my study profile, my plans, my contact networks among other things, I have mixed a lot of technologies here and I intend to continue doing my best to improve and fill out the website :3
+            </p>
+          </div>
+
+          <div className="mb-6 md:mb-0">
+            <h5 className="mb-2 font-medium uppercase">Other info</h5>
+            <p className="mb-4">
+            This site is hosted by Vercel, you can get the source code on my github, just don't judge my dirty code or my technical limitations, I still have the excuse of being a junior dev.
+            </p>
+          </div>
+        
         </div>
       </div>
     </footer>
