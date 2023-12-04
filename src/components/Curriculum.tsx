ed57814f05037a36ef2b01374c5c508aaff6cbfb
Aelys <AelysCh@proton.me>
2023-12-03 22:49:32 -0300

    Refactor About and Curriculum components

--- src/components/Curriculum.tsx
+++ src/components/Curriculum.tsx
@@ -1,9 +1,43 @@
-export default function Curriculum(){
+export default function Curriculum() {
   return (
-    <div className="bg-gray-200 p-4">
-      <h1 className="text-2xl font-bold mb-4">Curriculum</h1>
-      {/* Add your curriculum content here */}
+    <div className="animate-fade-in pl-5 flex justify-center items-start pt-10 bg-cover bg-fixed bg-[url('../mountain_sunset.jpg')]" style={{ minHeight: 'calc(100vh - 64px)' }}>
+      <div className="bg-gradient-to-b opacity-80 from-slate-800 to-transparent shadow-lg rounded-lg p-8 max-w-3xl mb-4">
+        <h1 className="text-3xl font-bold mb-6 text-center text-white">My curriculum</h1>
+        <h2 className="text-2xl font-bold mb-4 text-white">Experience</h2>
+    
+         <div className="mb-4">
+    <h3 className="text-xl font-bold text-white">Knowledge of Javascript and Typescript languages</h3>
+    <p className="text-white">I often work with various frameworks within Node, including Express, Fastify, Prisma
+ORM and Vite, which provide me with a good degree of familiarity with the
+languages and their respective characteristics.</p>
+
+  </div>
+         <div className="mb-4">
+    <h3 className="text-xl font-bold text-white">Familiarity with Linux operating systems</h3>
+    <p className="text-white">I use Linux daily as my primary operating system, which provides me with a high level
+of learning. I have knowledge in Bash scripting for development purposes</p>
+  </div> 
+
+  <div className="mb-4">
+    <h3 className="text-xl font-bold text-white">Basic Front-End experience</h3>
+    <p className="text-white">I often use frontend-focused frameworks (Tailwind CSS and React), which provide
+me with more flexibility during programming. As a result, I can create responsive and
+well-styled websites.</p>
+  </div> 
+
+  <div className="mb-4">
+    <h3 className="text-xl font-bold text-white">Other skills I have</h3>
+    <p className="text-white">Intermediate English (B1 level), Proficiency with Docker, virtual machines, version
+control (GIT), building Restful APIs, and occasional Python development for specific
+occasions.</p>
+  </div> 
+        
+
+       
+        
+      
+     
+      </div>
     </div>
   );
 }
-
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,14 +4,14 @@ export default function Footer() {
       <div className="container p-4 text-slate-200">
         <div className="grid gap-4 lg:grid-cols-2">
           <div className="mb-4 md:mb-0">
-            <h5 className="mb-2 text-sm font-medium uppercase">About this website</h5>
+            <h5 className="mb-2 text-sm pt-5 font-medium uppercase">About this website</h5>
             <p className="mb-4 mr-5 text-slate-300 font-light text-sm">
             The website has the purpose of bringing together my work, my study profile, my plans, my contact networks among other things, I have mixed a lot of technologies here and I intend to continue doing my best to improve and fill out the website :3
             </p>
           </div>
 
           <div className="mb-6 md:mb-0">
-            <h5 className="mb-2 text-sm font-medium uppercase">Other info</h5>
+            <h5 className="mb-2 pt-5 text-sm font-medium uppercase">Other info</h5>
             <p className="mb-4 ml-5 text-slate-300 font-light text-sm">
             This site is hosted by Vercel, you can get the source code on my github, just don't judge my dirty code or my technical limitations, I still have the excuse of being a junior dev.
             </p>
